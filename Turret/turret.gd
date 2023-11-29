c46627a5337458440b297884ef94241232f4e7ec
Declin-Anderson <Declin-Anderson@users.noreply.github.com>
2023-11-28 18:22:37 -0600

    Raytracing and turret information

---
+++ Turret/turret.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@export var projectile: PackedScene
+@onready var barrel: MeshInstance3D = $TurretBase/TurretTop/Visor/Barrel
+
+
+func _on_timer_timeout() -> void:
+	var shot = projectile.instantiate()
+	add_child(shot)
+	shot.global_position = barrel.global_position
